31d92264debe1c22b54aff2b8114c5e4342bec49
Alekseyka <harlan14@mail.ru>
2025-07-06 13:39:14 +0700

    Zadacha 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int summa = 121000;
-        int bonus = 20;
+        int nalichie = 100;
+        int popolnenie = 3500;
+        int bonus = 0;
+        if (popolnenie >= 1000) {
+            bonus = popolnenie / 100;
+        }
 
-        int bonusmyle = summa / bonus;
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
+        int finalBalance = nalichie + popolnenie + bonus;
 
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-        System.out.println(bonusmyle);
+        System.out.println(finalBalance);
+
     }
 }
