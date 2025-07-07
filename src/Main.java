f7517379ae14d59a40c01e12015d3b82fb5625ac
Alekseyka <harlan14@mail.ru>
2025-07-07 22:11:41 +0700

    change zadacha2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int nalichie = 100;
-        int popolnenie = 3500;
+        int cash = 100;
+        int replenishment = 3500;
         int bonus = 0;
-        if (popolnenie >= 1000) {
-            bonus = popolnenie / 100;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
         }
 
-        int finalBalance = nalichie + popolnenie + bonus;
+        int finalBalance = cash + replenishment + bonus;
 
         System.out.println(finalBalance);
 
